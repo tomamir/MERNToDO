0d5f2be043a310bdbe214a6a697b37f0414c03d9
tomamir <tomamir@Toms-MacBook-Pro.local>
2019-09-03 17:50:16 -0700

    Front end is up and running

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./media/logo.png";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand"  target="_blank">
+              <img src={logo} width="30" height="30"/>
+            </a>
+            <Link to="/" className="navbar-brand">To-Do Application</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
       </Router>
-        
-      
     );
   }
 }
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CreateTodo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Create Todo Component!!</p>
+            </div>
+        )
+    }
+}
